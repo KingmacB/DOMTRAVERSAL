4859e3a031e3207cb92c2d2598a730be5f5fd05d
Marc Beasley <beasleymac99@gmail.com>
2019-12-16 16:31:25 -0700

    initial commit

--- app/app.js
+++ app/app.js
@@ -9,3 +9,34 @@
  * THREE: write some JavaScript that finds ALL articles with a class="professor". Loop through each element and set the child
  * img element src to the relative clown file. Change the title to 'Clown', and subtitle to '@arrayClown' 
  */
+
+
+ let parent = document.getElementById('marc');
+ 
+ console.log(parent);
+
+ let title = parent.querySelector('.title');
+
+ console.log(title);
+
+ title.innerText = "Marc Beasley";
+
+ let subtitle = parent.querySelector ('.subtitle');
+
+ console.log(subtitle);
+
+ subtitle.innerText = "@MacBoiSlimYo";
+
+ let professors = document.querySelectorAll('.professor');
+ 
+ professors.forEach(professor => {
+    let eachProfessor = professor.querySelector('img');
+    eachProfessor.setAttribute('src', 'images/clown.jpg');
+    let professorTitle = professor.querySelector('.title');
+    professorTitle.innerText = "Clown";
+    let professorSubTitle = professor.querySelector('.subtitle');
+    professorSubTitle.innerText = "@Clown part ii";
+ }); 
+
+
+ 
